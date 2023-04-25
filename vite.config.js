4be02b63267217aac065bf5e7a5da48f4511ab47
Jhonattan Aponte <jfaponte403@gmail.com>
2023-04-24 19:58:56 -0500

    gh-pages add

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: { chunkSizeWarningLimit: 1600, },
-  plugins: [react()],
-  base: '/leaflet-react-QuickRide'
+  plugins: [react()]
 })
